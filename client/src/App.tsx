005a0f1598228c776eab976e6efcee95a72968d3
AgentBraun <AgentBraun@users.noreply.github.com>
2023-10-24 12:03:07 +0200

    project Init

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>TS AmazonC</header>
+      <main></main>
+      <footer>All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
